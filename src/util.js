fe8716dcb9191f6f58d959cc3d9bee2d62e554ba
Christopher Brown <io@henrian.com>
2021-01-25 17:02:15 -0500

    Replace EC2 instance with Cloudflare proxy to get SSL/TLS support
    Because modern browsers require that AJAX endpoints are secure (https)
    if the originating page is secure, and GitHub Pages mandates 'Enforce
    HTTPS' for sites using the default domain (i.e, *.github.io)

--- src/util.js
+++ src/util.js
@@ -1,6 +1,6 @@
-export const API_HOST = "ec2-52-14-57-65.us-east-2.compute.amazonaws.com"
+export const API_HOST = "news-backend.ttrend.in"
 
 export function fetchSummaries() {
-  const url = `http://${API_HOST}/summaries`
+  const url = `https://${API_HOST}/summaries`
   return fetch(url).then(response => response.json())
 }

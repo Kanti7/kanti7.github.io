1a57cf886b28724813f7861d8e9af9db163d6bd1
Christopher Brown <io@henrian.com>
2021-01-25 17:08:41 -0500

    Show a little more per article

--- src/components/article.js
+++ src/components/article.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react'
 
 function Article(props) {
-  const {summary_short, summary_long, n_articles} = props
+  const {summary_short, summary_long, total_articles} = props
 
   return (
     <article>
       <p>{summary_short}</p>
+      <details>
+        <summary>Long version</summary>
+        <p>{summary_long}</p>
+      </details>
+      <small>
+        ({total_articles} articles)
+      </small>
     </article>
   )
 }

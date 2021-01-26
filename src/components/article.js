eab27c1aa5d58a1011f8907538f9e6a63ff28577
Christopher Brown <io@henrian.com>
2021-01-26 12:08:16 -0500

    Sharpen transition of image overlay

--- src/components/article.js
+++ src/components/article.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
 
-const TOP_WHITE = 'rgba(100%, 100%, 100%, 60%)'
-const BOTTOM_WHITE = 'rgba(100%, 100%, 100%, 80%)'
+const TOP_WHITE = 'rgba(100%, 100%, 100%, 70%)'
+const BOTTOM_WHITE = 'rgba(100%, 100%, 100%, 90%)'
 
 function Article(props) {
   const {id, summary_short, summary_long, first_article, total_articles} = props
   const image_url = first_article.newsapi_image_url
 
   const style = {
-    background: `linear-gradient(${TOP_WHITE}, ${BOTTOM_WHITE}), center/cover no-repeat url(${image_url})`,
+    background: `linear-gradient(transparent 24%, ${TOP_WHITE} 48%, ${BOTTOM_WHITE}), center/cover no-repeat url(${image_url})`,
   }
 
   return (

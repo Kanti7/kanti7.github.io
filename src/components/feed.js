69842e7dac48347bf7037287c31aa7f35d1f890f
Christopher Brown <io@henrian.com>
2021-01-26 13:25:52 -0500

    Add pagination to feed

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 import {fetchSummaries} from '../util'
 
 import Article from './article'
 
 function Feed() {
+  const location = useLocation()
+  const query = new URLSearchParams(location.search);
+
   const [summaries, setSummaries] = useState([])
 
   useEffect(() => {
     fetchSummaries().then(summaries => setSummaries(summaries))
   }, [])
 
-  const displayed_summaries = summaries.slice(0, 10)
+  const offset = parseInt(query.get("offset") || 0, 10)
+  const limit = parseInt(query.get("limit") || 20, 10)
+  const displayed_summaries = summaries.slice(offset, offset + limit)
 
   return (
     <div className="feed">
@@ -25,6 +31,11 @@ function Feed() {
           <Article {...summary} />
         </div>
       )}
+      <footer>
+        <Link to={{search: `?offset=${offset + limit}&limit=${limit}`}}>
+          View more
+        </Link>
+      </footer>
     </div>
   )
 }

0a2b314dbbf2e0585c0b632e6eb025a431b4c153
Christopher Brown <io@henrian.com>
2021-02-15 08:30:02 -0500

    Migrate URLs to use '/v1' prefix

--- src/util.js
+++ src/util.js
@@ -1,11 +1,11 @@
 export const API_HOST = "news-backend.ttrend.in"
 
 export function fetchSummaries() {
-  const url = `https://${API_HOST}/summaries`
+  const url = `https://${API_HOST}/v1/summaries`
   return fetch(url).then(response => response.json())
 }
 
 export function fetchSummary(stem, index) {
-  const url = `https://${API_HOST}/summaries/${stem}/${index}`
+  const url = `https://${API_HOST}/v1/summaries/${stem}/${index}`
   return fetch(url).then(response => response.json())
 }
